49361ff043742fa393443129abb980101b1a5bcc
hakkeriino <30336435+hakkeriino@users.noreply.github.com>
2017-11-21 18:09:37 +0200

    Update compare.sh
    Lisättiin kommentti ja kuvaus skriptiin

--- videowall/compare.sh
+++ videowall/compare.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+#vertaa output.txt tiedostoa ja vertaa sitä pyörivien palvelujen kanssa. Jos tiedostossa on eroa se lisätään output.txt tiedostoon, jos muutoksia ei ole tiedosto pysyy samana
+
 diff <(cat output.txt) <(service --status-all | grep + -C 0) >> output.txt
 
